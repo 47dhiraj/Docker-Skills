3b486ed63b7c4974d75ddd4c10d73ce0a7976d2a
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-06-27 23:19:51 +0545

    Dockerize Django with MySQL

--- Dockerize_Django_and_React/2. Docker Compose _ work in docker container/core/core/settings.py
+++ Dockerize_Django_and_React/2. Docker Compose _ work in docker container/core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g6j!3kmk!ug+hqzrxdgakd4r72#5hulj^*z_*r@0d=22u)+!xc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
